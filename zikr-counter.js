6552002f667d23e2fa912fbeeff39a202de96d7d
muyeedsarker <sarkermuyeed@gmail.com>
2026-09-08 11:10:00 +0600

    Show external zikr counter only on Tasbih page

--- zikr-counter.js
+++ zikr-counter.js
@@ -1,7 +1,9 @@
 (function(){
+  if(!/\/tasbih\.html(?:$|\?)/.test(location.pathname))return;
   const adhkar=['সুবহানাল্লাহ','আলহামদুলিল্লাহ','আল্লাহু আকবার','লা ইলাহা ইল্লাল্লাহ','আস্তাগফিরুল্লাহ','দরুদ শরীফ'];
   const targets=[33,100,300,500,1000];
   function init(){
+    if(!/\/tasbih\.html(?:$|\?)/.test(location.pathname))return;
     if(document.getElementById('sohochor-zikr-counter'))return;
     const box=document.createElement('section');box.id='sohochor-zikr-counter';box.innerHTML='<div class="sc-z-title">🤲 তসবীহ / যিকির</div><select id="sc-zikr" aria-label="যিকির নির্বাচন">'+adhkar.map(x=>'<option>'+x+'</option>').join('')+'</select><select id="sc-target" aria-label="লক্ষ্য নির্বাচন">'+targets.map(x=>'<option value="'+x+'">'+x+' বার</option>').join('')+'<option value="custom">নিজের সংখ্যা</option></select><input id="sc-custom" type="number" min="1" max="100000" placeholder="নিজের সংখ্যা" hidden><div class="sc-z-count" id="sc-count">0</div><button id="sc-add" type="button">+ ১</button><button id="sc-reset" type="button">রিসেট</button><div id="sc-status">Target: 33</div></div>';
     const anchor=document.querySelector('.hero')||document.body.firstElementChild;if(anchor&&anchor.parentNode)anchor.parentNode.insertBefore(box,anchor.nextSibling);else document.body.prepend(box);
